07c72269844039ed3d2f7da34eecad7daa396807
tuonybt <tonybt.tbt@gmail.com>
2023-04-28 15:47:08 +0100

    Found all complete paths

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,55 @@ fn main() {
 
     for ball_order in ball_orders.iter() {
         adj_pairs.insert(ball_order, ball_swap(ball_order.to_owned()).0);
-        nadj_pairs.insert(ball_order, ball_swap(ball_order.to_owned()).0);
+        nadj_pairs.insert(ball_order, ball_swap(ball_order.to_owned()).1);
 
     }
-    println!("Adjacent neighbour pairs {:?}", adj_pairs);
-    println!("Non-adjacent neighbour pairs {:?}", nadj_pairs);
+
+    for node in ball_orders.iter() {
+//        println!("Node {:?}", node);
+//        println!("Adjacent neighbour pairs {:?}", adj_pairs[node]);
+//        println!("Non-adjacent neighbour pairs {:?}", nadj_pairs[node]);
+//        println!();
+
+        let mut new_edges = vec![vec![node]];
+        for pass in 1..13 {
+
+            let mut full_paths = Vec::<Vec<&Vec<&str>>>::new();
+            for start in &new_edges {
+//                println!("Start {:?}", start.last().unwrap());
     
+                let nbrs = match pass %5 {
+                    0 => &nadj_pairs[start.last().unwrap()],
+                    _ => &adj_pairs[start.last().unwrap()],
+                };
+                full_paths.extend(edges(start.to_owned(), nbrs));
+            }
+            new_edges = full_paths.to_owned();
+            
+            if pass == 11 && full_paths.len() > 0 {    
+                println!("Node {:?}", node);
+                println!("New edges found on pass {}: {:?}", pass, full_paths);
+//                println!("New edges found on pass {}: {:?}", pass, full_paths.iter().map(|z| z.len()).collect::<Vec<usize>>());
+                println!();
+            }
+        }
 
+    }
 
+}
+
+fn edges<'a>(seq: Vec<&'a Vec<&str>>, nbrs: &'a BTreeSet<Vec<&str>>) -> Vec<Vec<&'a Vec<&'a str>>> {
+
+    let mut new_paths = Vec::<Vec<&Vec<&str>>>::new();
+    let avail_nbrs = nbrs.iter().filter(|z| !seq.contains(z));
+
+    for nbr in avail_nbrs {
+        let mut new_path = seq.to_owned();
+        new_path.push(nbr);
+        new_paths.push(new_path);
+    }
+
+    new_paths
 }
 
 fn ball_swap(fr: Vec<&str>) -> (BTreeSet<Vec<&str>>, BTreeSet<Vec<&str>>) {
@@ -31,7 +72,9 @@ fn ball_swap(fr: Vec<&str>) -> (BTreeSet<Vec<&str>>, BTreeSet<Vec<&str>>) {
 
     for (id, ball) in fr.iter().enumerate() {
         for (idx, swap) in fr[id + 1..].iter().enumerate() {
-            if ball == &"blue" && swap == &"black"  {continue}
+//            if ball == &"blue" && swap == &"black"  {continue}
+            if ball == &"blue" && idx >= fr[id + 1..].iter().position(|z| z == &"black").unwrap()  {continue}
+            if swap == &"black" && id < fr.iter().position(|z| z == &"blue").unwrap()  {continue}
             let mut new = fr.clone();
             new[id] = swap;
             new[id + idx + 1] = ball;
